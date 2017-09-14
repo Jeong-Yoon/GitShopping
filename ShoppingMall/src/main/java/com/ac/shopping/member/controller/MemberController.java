766ffddab357ed68e9bb8f82a3598c5e3b94eaaa
hyunjin <hyunjin@gmail.com>
2017-09-14 16:37:36 +0900

    hj

--- ShoppingMall/src/main/java/com/ac/shopping/member/controller/MemberController.java
+++ ShoppingMall/src/main/java/com/ac/shopping/member/controller/MemberController.java
@@ -154,8 +154,6 @@ public class MemberController {
     	PrintWriter out = response.getWriter();
     	out.println("<script>alert('로그아웃 되셨습니다.'); document.location.href='shoppingindex'</script>");
     	out.close();
-        mav.setViewName("shoppingindex");
-        mav.addObject("msg", "logout");
         return mav;
     }
     
